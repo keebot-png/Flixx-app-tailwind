7546b308f0d6a011da05cf631f595c985ca0a7f6
keebot-png <kee.bot@icloud.com>
2023-05-18 21:06:50 +0200

    Add navbar and Tailwind cdn.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,55 @@
-import message from "./message";
 import './css/style.css';
+const location = {
+   currentPage: window.location.pathname
+}
 
-console.log(message);
+//display popular movies
+const displayMovies = async() => {
+    const result = await fetchData('./movie-details.html')
+    return result;
+}
 
-const hello = () => console.log('hello');
+
+//fetch api data
+const fetchData = async(LINK) => {
+    API_KEY = '';
+    API_URL = 'https://api.themoviedb.org/3/';
+    const response = await fetch(`${API_URL}${LINK}?api_key=${API_KEY}&language=en-US`)
+    const data = await response.json();
+    return data;
+}
+
+// highlighting the active link
+const highlightLink = () => {
+    //get links
+    const links = document.querySelectorAll('nav-link');
+    links.forEach(link => {
+        if(link.getAttribute('href') === location.currentPage){
+            link.classList.add('active');
+        }
+    })
+}
+
+const init = () => {
+    switch(location.currentPage){
+        case '/':
+        case '/index.html':
+            console.log('home');
+            break;
+        case '/shows.html':
+            console.log('shows');
+            break;
+        case '/movie-details.html':
+            displayMovies();
+            break;
+        case '/tv-details.html':
+            console.log('TV details')
+            break;
+        case '/search.html':
+            console.log('Search')
+            break;
+    }
+    highlightLink()
+}
+
+document.addEventListener('DOMContentLoaded', init);
